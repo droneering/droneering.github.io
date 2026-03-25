d706a936301b6c4266f6eb1de2df433fe9bb3074
droneering <droneeringai@gmail.com>
2026-03-25 17:56:14 +0900

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Technology from './pages/Technology';
 import Impact from './pages/Impact';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import AiEthics from './pages/AiEthics';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +15,8 @@ const App: React.FC = () => {
           <Route index element={<Home />} />
           <Route path="technology" element={<Technology />} />
           <Route path="impact" element={<Impact />} />
+          <Route path="privacy" element={<PrivacyPolicy />} />
+          <Route path="ai-ethics" element={<AiEthics />} />
         </Route>
       </Routes>
     </BrowserRouter>
